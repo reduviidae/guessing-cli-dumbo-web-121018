05340e637c6976d52d0ed73ec2c1d7d93f984d8b
Wil Cook <wil.cook.is@gmail.com>
2018-12-01 21:25:35 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,14 @@
-# Code your solution here!
+require "pry"
+
+def run_guessing_game
+  puts "Guess a number between 1 and 6."
+  correct = rand(1..6)
+  guess = gets.chomp.to_i
+  # binding.pry
+  if correct == guess 
+    puts "You guessed the correct number!"
+  else 
+    puts "The computer guessed #{correct}."
+  end
+  
+end
